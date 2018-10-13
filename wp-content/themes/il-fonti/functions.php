556d2c68f07224bb971654d257d82614e3845041
Pame <39319136+sundarkaur@users.noreply.github.com>
2018-10-13 00:03:59 -0300

    Update functions.php

--- wp-content/themes/il-fonti/functions.php
+++ wp-content/themes/il-fonti/functions.php
@@ -28,6 +28,8 @@
 
   add_action('wp_enqueue_scripts', 'register_enqueue_style' );
 
+
+
    // Registro de scripts //
  function register_enqueue_scripts() {
  	 $theme_data = wp_get_theme();
@@ -53,6 +55,100 @@
 
 
  add_action('wp_enqueue_scripts', 'register_enqueue_scripts' );
+
+  remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );
+
+
+
+/**
+* Remove Elements From Menu
+* @return  void
+* @since   1.0
+* @uses    default disabled
+* @see     https://codex.wordpress.org/Plugin_API/Action_Reference/admin_menu
+*/
+function dl_remove_menus(){
+
+
+  // remove_menu_page( 'index.php' );                //Dashboard
+  remove_menu_page( 'edit.php' );                    //Post
+  // remove_menu_page( 'upload.php' );               //Media
+  // remove_menu_page( 'edit.php?post_type=page' );  //Pages
+  remove_menu_page( 'edit-comments.php' );           //comments
+  // remove_menu_page( 'themes.php' );               //Appearence               
+  // remove_menu_page( 'plugins.php' );              //Plugins
+  // remove_menu_page( 'users.php' );                //Users
+  // remove_menu_page( 'tools.php' );                //Tools
+  // remove_menu_page( 'options-general.php' );      //Settings
+
+}
+
+add_action( 'admin_menu' , 'dl_remove_menus' );
+
+
+
+
+if ( ! function_exists('about_CPT') ) {
+
+// Register Custom Post Type
+function about_CPT() {
+
+  $labels = array(
+    'name'                  => _x( 'Nosotros', 'Post Type General Name', 'text_domain' ),
+    'singular_name'         => _x( 'Nosotros', 'Post Type Singular Name', 'text_domain' ),
+    'menu_name'             => __( 'Nosotros', 'text_domain' ),
+    'name_admin_bar'        => __( 'Nosotros', 'text_domain' ),
+    'archives'              => __( 'Item Archives', 'text_domain' ),
+    'attributes'            => __( 'Item Attributes', 'text_domain' ),
+    'parent_item_colon'     => __( 'Parent Item:', 'text_domain' ),
+    'all_items'             => __( 'All Items', 'text_domain' ),
+    'add_new_item'          => __( 'Add New Item', 'text_domain' ),
+    'add_new'               => __( 'Add New', 'text_domain' ),
+    'new_item'              => __( 'New Item', 'text_domain' ),
+    'edit_item'             => __( 'Edit Item', 'text_domain' ),
+    'update_item'           => __( 'Update Item', 'text_domain' ),
+    'view_item'             => __( 'View Item', 'text_domain' ),
+    'view_items'            => __( 'View Items', 'text_domain' ),
+    'search_items'          => __( 'Search Item', 'text_domain' ),
+    'not_found'             => __( 'Not found', 'text_domain' ),
+    'not_found_in_trash'    => __( 'Not found in Trash', 'text_domain' ),
+    'featured_image'        => __( 'Featured Image', 'text_domain' ),
+    'set_featured_image'    => __( 'Set featured image', 'text_domain' ),
+    'remove_featured_image' => __( 'Remove featured image', 'text_domain' ),
+    'use_featured_image'    => __( 'Use as featured image', 'text_domain' ),
+    'insert_into_item'      => __( 'Insert into item', 'text_domain' ),
+    'uploaded_to_this_item' => __( 'Uploaded to this item', 'text_domain' ),
+    'items_list'            => __( 'Items list', 'text_domain' ),
+    'items_list_navigation' => __( 'Items list navigation', 'text_domain' ),
+    'filter_items_list'     => __( 'Filter items list', 'text_domain' ),
+  );
+  $args = array(
+    'label'                 => __( 'Nosotros', 'text_domain' ),
+    'description'           => __( 'Post Type Description', 'text_domain' ),
+    'labels'                => $labels,
+    'supports'              => array( 'title', 'editor', 'thumbnail' ),
+    'taxonomies'            => array(),
+    'hierarchical'          => false,
+    'public'                => true,
+    'show_ui'               => true,
+    'show_in_menu'          => true,
+    'menu_position'         => 5,
+    'menu_icon'             => 'dashicons-groups',
+    'show_in_admin_bar'     => true,
+    'show_in_nav_menus'     => true,
+    'can_export'            => true,
+    'has_archive'           => true,
+    'exclude_from_search'   => false,
+    'publicly_queryable'    => true,
+    'capability_type'       => 'page',
+  );
+  register_post_type( 'about', $args );
+
+}
+add_action( 'init', 'about_CPT', 0 );
+
+}
+
 
 
 
